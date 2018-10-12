4f944bf66b0f0023676fce17cf772486d105afb9
kawsark <kawsar@hashicorp.com>
2018-10-11 22:26:31 -0400

    Corrected terraform block

--- s3-backend.tf
+++ s3-backend.tf
@@ -1,5 +1,7 @@
-backend "s3" {
-    bucket = "kawsar-tfstate-061318"
-    key    = "tfe-state/git/kawsark/terraform-aws-ec2-instance/dev"
-    region = "us-east-1"
+terraform {
+  backend "s3" {
+      bucket = "kawsar-tfstate-061318"
+      key    = "tfe-state/git/kawsark/terraform-aws-ec2-instance/dev"
+      region = "us-east-1"
+  }
 }
