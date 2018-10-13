646123731213c374bd1535d3fc548d1d086330d7
kawsark <kawsar@hashicorp.com>
2018-10-13 18:30:20 -0400

    Added example vault server

---
+++ examples/vault-server/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Render userdata
+data "template_file" "startup_script" {
+  template = "${file("${path.module}/vault-consul.sh.tpl")}"
+  vars{
+    CONSUL_VERSION = "${var.consul_version}"
+    VAULT_VERSION = "${var.vault_version}"
+  }
+}
+
+# Consul server 1
+module "vault-server" {
+  source    = "github.com/kawsark/terraform-aws-ec2-instance?ref=userdata"
+  name      = "demo-vault-server"
+  owner     = "${var.owner}"
+  count     = "1"
+  key_name  = "${var.key_name}"
+  user_data = "${data.template_file.startup_script.rendered}"
+  subnet_id = "${aws_subnet.mvd-public-1.id}"
+  sg_ids    = ["${aws_security_group.mvd-sg.id}"]
+  private_ip = "${lookup(var.private_ip_map,"n1")}"
+  sequence = "0"
+}
