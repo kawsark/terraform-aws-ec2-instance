53ac32ce16927536b6eb3836a0ed8a4adaddf616
kawsark <kawsar@hashicorp.com>
2018-10-14 23:15:53 -0400

    fixed cloudflare value

--- examples/vault-server/cloudflare.tf
+++ examples/vault-server/cloudflare.tf
@@ -3,7 +3,7 @@ resource "cloudflare_record" "vault_server" {
   count  = "${var.create_cloudflare_dns}"
   domain = "${var.cloudflare_domain}"
   name   = "vault"
-  value  = "${module.vault-server.public_dns}"
+  value  = "${module.vault-server.public_dns[0]}"
   type   = "CNAME"
   ttl    = 1
 }
