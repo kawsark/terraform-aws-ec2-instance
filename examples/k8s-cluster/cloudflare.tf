0e4b4fbd22d0b3f0ef08240531b6379aef64a71f
kawsark <kawsar@hashicorp.com>
2019-01-13 23:54:22 -0500

    moving demo-hashistack to its own branch

--- examples/k8s-cluster/cloudflare.tf
+++
@@ -1,9 +0,0 @@
-# Add the server record to domain
-resource "cloudflare_record" "aws-kubernetes" {
-  count  = "${var.create_cloudflare_dns}"
-  domain = "${var.cloudflare_domain}"
-  name   = "k8s"
-  value  = "${module.aws-kubernetes.public_dns[0]}"
-  type   = "CNAME"
-  ttl    = 1
-}
--- examples/vault-server/cloudflare.tf
+++
@@ -1,9 +0,0 @@
-# Add the server record to domain
-resource "cloudflare_record" "vault_server" {
-  count  = "${var.create_cloudflare_dns}"
-  domain = "${var.cloudflare_domain}"
-  name   = "vault"
-  value  = "${module.vault-server.public_dns[0]}"
-  type   = "CNAME"
-  ttl    = 1
-}
