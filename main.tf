71925399c1218becca9107fa70d9abb5ca8b0d3f
kawsark <kawsar@hashicorp.com>
2020-01-28 13:47:25 -0500

    updated module to 0.12

--- main.tf
+++ main.tf
@@ -3,19 +3,19 @@ terraform {
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  count	 	= "${var.count}"
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
+  count             = var.num_servers
+  ami               = var.ami_id
+  instance_type     = var.instance_type
   availability_zone = "${var.aws_region}a"
 
-  tags {
-    Name = "${var.name}-${count.index}"
-    owner = "${var.owner}"
-    TTL = "${var.ttl}"
+  tags = {
+    Name  = "${var.name}-${count.index}"
+    owner = var.owner
+    TTL   = var.ttl
   }
-
 }
+
